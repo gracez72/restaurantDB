cbb999daa88bd364edd3e103ca8c3a261b763859
diannakan1998 <diannakan1998@gmail.com>
2019-03-24 14:38:43 -0700

    tt

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/main', function (req, res) {
+  res.render('main', { title: 'Express' });
+});
 router.post('/search', function (req, res) {
   console.log(req.body.search + " searched ");
-  res.redirect('/');
+  res.redirect('/main');
 });
 
+
+router.post('/login', function (req, res) {
+  console.log(req.body.AccName+ " "+ req.body.Name +" " + req.body.password);
+  res.redirect('/main');
+});
+router.post('/register', function (req, res) {
+  console.log(req.body.AccName+ " "+ req.body.Name +" " + req.body.password);
+  res.redirect('/main');
+});
 module.exports = router;
