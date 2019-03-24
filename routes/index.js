ca5b85745cbee3b6793769099fba598853e1982c
zengxueqi <zengxueqi1997@g,ail.com>
2019-03-24 15:45:58 -0700

    add restaurants employees

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,76 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('employees', { title: 'Express' });
 });
 
 router.get('/main', function (req, res) {
   res.render('main', { title: 'Express' });
 });
-router.post('/search', function (req, res) {
-  console.log(req.body.search + " searched ");
-  res.redirect('/main');
+
+router.get('/restaruant', function (req, res) {
+  res.render('restaruant', { title: 'Express' });
 });
 
+router.get('/employees', function (req, res) {
+  res.render('employees', { title: 'Express' });
+});
+
+router.post('/ingredient', function (req, res) {
+  console.log("View Ingredients");
+  res.redirect('/employees');
+});
+
+router.post('/pay', function (req, res) {
+  console.log("View Average Pay");
+  res.redirect('/employees');
+});
+
+router.post('/expiry', function (req, res) {
+  console.log("View Expiry Date of Ingredients");
+  res.redirect('/employees');
+});
+
+router.post('/purchase', function (req, res) {
+  console.log(req.body.name + " " + req.body.amount + "purchase");
+  res.redirect('/employees');
+});
+
+router.post('/use', function (req, res) {
+  console.log(req.body.name + " " + req.body.amount + "use");
+  res.redirect('/employees');
+});
+
+
+router.post('/name', function (req, res) {
+  console.log(req.body.name + " name ");
+  res.redirect('/restaruant');
+});
+
+router.post('/type', function (req, res) {
+  console.log(req.body.type + " type ");
+  res.redirect('/restaruant');
+});
+
+router.post('/rating', function (req, res) {
+  console.log(req.body.rating + " rating ");
+  res.redirect('/restaruant');
+});
+
+router.post('/review', function (req, res) {
+  console.log(req.body.review + " review ");
+  res.redirect('/restaruant');
+});
+
+router.post('/allergy', function (req, res) {
+  console.log(req.body.review + " allergy ");
+  res.redirect('/restaruant');
+});
+
+router.post('/cuisine', function (req, res) {
+  console.log(req.body.cuisine + " cuisine ");
+  res.redirect('/main');
+});
 
 router.post('/login', function (req, res) {
   console.log(req.body.AccName+ " "+ req.body.Name +" " + req.body.password);
